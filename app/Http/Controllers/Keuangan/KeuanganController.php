0444768fad3914030b38a3cf7775878fb745f961
FieryInferno <bagassetia271@gmail.com>
2021-07-28 19:52:53 +0700

    Penambahan grafik pengadaan di keuangan

--- app/Http/Controllers/Keuangan/KeuanganController.php
+++ app/Http/Controllers/Keuangan/KeuanganController.php
@@ -8,9 +8,26 @@ use App\Models\Pengadaan;
 
 class KeuanganController extends Controller
 {
+  private $pengadaan;
+  
+  public function __construct()
+  {
+    $this->pengadaan  = new Pengadaan;
+  }
 
   public function index()
   {
-    return view('keuangan/dashboard');
+    $tgl_awal       = date('Y')-10;
+    $tgl_akhir      = date('Y')+3;
+    $data_pengadaan = [];
+    for ($i = $tgl_awal; $i <= $tgl_akhir; $i++) {
+      $pengadaan          = $this->pengadaan->whereYear('tanggal_input', $i)->get();
+      $data_pengadaan[$i] = 0;
+      foreach ($pengadaan as $key) {
+        $data_pengadaan[$i] += $key['harga_aset'];
+      }
+    }
+    $pengadaan['data_pengadaan']  = $data_pengadaan;
+    return view('keuangan/dashboard', $pengadaan);
   }
 }
