045e50bad43cb1b5e6be259be8c7239f9524e477
FieryInferno <bagassetia271@gmail.com>
2021-07-28 20:46:00 +0700

    Perbaikan dashboard kaur laboratorium

--- app/Http/Controllers/KaurLaboratorium/KaurLaboratoriumController.php
+++ app/Http/Controllers/KaurLaboratorium/KaurLaboratoriumController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\KaurLaboratorium;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Asets;
 
 class KaurLaboratoriumController extends Controller
 {
+  private $aset;
+  
+  public function __construct()
+  {
+    $this->aset = new Asets;
+  }
 
   public function index()
   {
-    return view('kaur_laboratorium/dashboard');
+    $data['total']        = $this->aset->count();
+    $data['baik']         = $this->aset->where('kondisi', 'baik')->count();
+    $data['maintenance']  = $this->aset->where('kondisi', 'maintenance')->count();
+    $data['rusak']        = $this->aset->where('kondisi', 'rusak')->count();
+    return view('kaur_laboratorium/dashboard', $data);
   }
 }
--- resources/views/kaur_laboratorium/dashboard.blade.php
+++ resources/views/kaur_laboratorium/dashboard.blade.php
@@ -1,4 +1,79 @@
 @extends('template')
 @section('konten')
-
+<div class="container-fluid mt-3">
+  <div class="row">
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Barang</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $total; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                <i class="ni ni-active-40"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Barang Kondisi Baik</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $baik; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                <i class="ni ni-chart-pie-35"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Barang Kondisi Rusak</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $rusak; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                <i class="ni ni-money-coins"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Barang Kondisi Maintenance</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $maintenance; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-info text-white rounded-circle shadow">
+                <i class="ni ni-chart-bar-32"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
