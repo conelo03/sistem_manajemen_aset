f8e56e1cf4bb396543a7df7ac6000642a1f0306c
FieryInferno <bagassetia271@gmail.com>
2021-08-01 21:34:51 +0700

    Penambahan data peminjaman pada dashboard laboran dan kaur lab

--- app/Http/Controllers/Laboran/LaboranController.php
+++ app/Http/Controllers/Laboran/LaboranController.php
@@ -5,22 +5,28 @@ namespace App\Http\Controllers\Laboran;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Asets;
+use App\Models\Peminjaman;
 
 class LaboranController extends Controller
 {
   private $aset;
+  private $peminjaman;
   
   public function __construct()
   {
-    $this->aset = new Asets;
+    $this->aset       = new Asets;
+    $this->peminjaman = new Peminjaman;
   }
 
   public function index()
   {
-    $data['total']        = $this->aset->count();
-    $data['baik']         = $this->aset->where('kondisi', 'baik')->count();
-    $data['maintenance']  = $this->aset->where('kondisi', 'maintenance')->count();
-    $data['rusak']        = $this->aset->where('kondisi', 'rusak')->count();
+    $data['total']              = $this->aset->count();
+    $data['baik']               = $this->aset->where('kondisi', 'baik')->count();
+    $data['maintenance']        = $this->aset->where('kondisi', 'maintenance')->count();
+    $data['rusak']              = $this->aset->where('kondisi', 'rusak')->count();
+    $data['peminjaman_selesai'] = $this->peminjaman->where('status', 'selesai')->count();
+    $data['peminjaman_hilang']  = $this->peminjaman->where('status', 'hilang')->count();
+    $data['peminjaman_rusak']   = $this->peminjaman->where('status', 'rusak')->count();
     return view('laboran/dashboard', $data);
   }
 }
--- resources/views/laboran/dashboard.blade.php
+++ resources/views/laboran/dashboard.blade.php
@@ -75,5 +75,61 @@
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="col-xl-4 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Peminjaman Selesai</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $peminjaman_selesai; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
+                <i class="ni ni-check-bold"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Peminjaman Hilang</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $peminjaman_hilang; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
+                <i class="ni ni-fat-delete"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-4 col-md-6">
+      <div class="card card-stats">
+        <!-- Card body -->
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <h5 class="card-title text-uppercase text-muted mb-0">Jumlah Peminjaman Rusak</h5>
+              <span class="h2 font-weight-bold mb-0"><?= $peminjaman_rusak; ?></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
+                <i class="ni ni-settings"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
