8c7bbf944cc60bc695cb60fb6d0a5a19389aa0c5
FieryInferno <bagassetia271@gmail.com>
2021-08-06 21:41:35 +0700

    Perbaikan error ggrafik

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -411,28 +411,28 @@
       $('#myTable').DataTable();
 
       $('.mySelect2').select2();
+      
+      var rupiah = document.getElementById('rupiah');
+      rupiah.addEventListener('keyup', function(e){
+        rupiah.value = formatRupiah(this.value, 'Rp. ');
+      });
+      
+      function formatRupiah(angka, prefix){
+        var number_string = angka.replace(/[^,\d]/g, '').toString(),
+        split   		      = number_string.split(','),
+        sisa     		      = split[0].length % 3,
+        rupiah     		    = split[0].substr(0, sisa),
+        ribuan     		    = split[0].substr(sisa).match(/\d{3}/gi);
+        
+        if(ribuan){
+          separator = sisa ? '.' : '';
+          rupiah    += separator + ribuan.join('.');
+        }
+        
+        rupiah  = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+      }
     });
-
-    var rupiah = document.getElementById('rupiah');
-		rupiah.addEventListener('keyup', function(e){
-			rupiah.value = formatRupiah(this.value, 'Rp. ');
-		});
-    
-		function formatRupiah(angka, prefix){
-			var number_string = angka.replace(/[^,\d]/g, '').toString(),
-			split   		      = number_string.split(','),
-			sisa     		      = split[0].length % 3,
-			rupiah     		    = split[0].substr(0, sisa),
-			ribuan     		    = split[0].substr(sisa).match(/\d{3}/gi);
-      
-			if(ribuan){
-				separator = sisa ? '.' : '';
-				rupiah    += separator + ribuan.join('.');
-			}
-      
-			rupiah  = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-			return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-		}
     
     // Set new default font family and font color to mimic Bootstrap's default styling
     Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
