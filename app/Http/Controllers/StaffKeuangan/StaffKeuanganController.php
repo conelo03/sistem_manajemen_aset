180065bfe23f25d057a0e00862a4140310c539c1
FieryInferno <bagassetia271@gmail.com>
2021-09-24 06:39:56 +0700

    Mengganti chart pada dashboard staff keuangan

--- app/Http/Controllers/StaffKeuangan/StaffKeuanganController.php
+++ app/Http/Controllers/StaffKeuangan/StaffKeuanganController.php
@@ -5,29 +5,40 @@ namespace App\Http\Controllers\StaffKeuangan;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Pengadaan;
+use App\Models\Maintenance;
 
 class StaffKeuanganController extends Controller
 {
   private $pengadaan;
+  private $maintenance;
   
   public function __construct()
   {
-    $this->pengadaan  = new Pengadaan;
+    $this->pengadaan    = new Pengadaan;
+    $this->maintenance  = new Maintenance;
   }
 
   public function index()
   {
-    $tgl_awal       = date('Y')-10;
-    $tgl_akhir      = date('Y')+3;
-    $data_pengadaan = [];
-    for ($i = $tgl_awal; $i <= $tgl_akhir; $i++) {
-      $pengadaan          = $this->pengadaan->whereYear('tanggal_input', $i)->get();
-      $data_pengadaan[$i] = 0;
-      foreach ($pengadaan as $key) {
-        $data_pengadaan[$i] += $key['harga_aset'];
-      }
-    }
-    $pengadaan['data_pengadaan']  = $data_pengadaan;
-    return view('staff_keuangan/dashboard', $pengadaan);
+    $collection   = collect($this->pengadaan->get());
+
+    $aset = $collection->map(function ($item) {
+      return $item['harga_aset'] * $item['quantity'];
+    });
+
+    $data['p_realisasi']  = $aset->sum();
+    $data['p_anggaran']   = 10000000000;
+    $data['p_pengadaan']  = $aset->sum() / 10000000000 * 100;
+
+    $collection   = collect($this->maintenance->get());
+
+    $maintenance  = $collection->map(function ($item) {
+      return $item['biaya'];
+    });
+
+    $data['m_realisasi']    = $maintenance->sum();
+    $data['m_anggaran']     = 10000000000;
+    $data['m_maintenance']  = $maintenance->sum() / 1000000000 * 100;
+    return view('staff_keuangan/dashboard', $data);
   }
 }
--- resources/views/staff_keuangan/dashboard.blade.php
+++ resources/views/staff_keuangan/dashboard.blade.php
@@ -2,19 +2,98 @@
 @section('konten')
 <div class="container-fluid mt-3">
   <div class="row">
-    <div class="col-xl-8">
-      <div class="card bg-default">
-        <div class="card-header bg-transparent">
-          <div class="row align-items-center">
-            <div class="col">
-              <h6 class="text-light text-uppercase ls-1 mb-1">Total Pengadaan</h6>
+    <div class="col-12">
+      <div class="row">
+        <div class="col-6">
+          <div class="card">
+            <div class="card-header">Pengeluaran Pengadaan</div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <div class="svg-item">
+                    <svg width="350%" height="100%" viewBox="0 0 40 40" class="donut">
+                      <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+                      <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+                      <circle class="donut-segment donut-segment-2" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="{{ $p_pengadaan . ' ' . (100 - $p_pengadaan) }}" stroke-dashoffset="25"></circle>
+                      <g class="donut-text donut-text-1">
+                        <text y="50%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-percent">{{ round($p_pengadaan, 2) }}%</tspan>   
+                        </text>
+                        <text y="60%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-data">% dari Anggaran Pengadaan</tspan>   
+                        </text>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+                <div class="col text-center">
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Realisasi</div>
+                      <div><strong>{{ rupiah($p_realisasi) }}</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Anggaran</div>
+                      <div><strong>{{ rupiah($p_anggaran) }}</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Selisih</div>
+                      <div><strong>{{ rupiah($p_anggaran - $p_realisasi) }}</strong></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="card-body">
-          <div class="chart">
-            <!-- Chart wrapper -->
-            <canvas id="myBarChart" class="chart-canvas"></canvas>
+        <div class="col-6">
+          <div class="card">
+            <div class="card-header">Pengeluaran Maintenance</div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <div class="svg-item">
+                    <svg width="350%" height="100%" viewBox="0 0 40 40" class="donut">
+                      <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+                      <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+                      <circle class="donut-segment donut-segment-2" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="{{ $m_maintenance . ' ' . (100 - $m_maintenance) }}" stroke-dashoffset="25"></circle>
+                      <g class="donut-text donut-text-1">
+                        <text y="50%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-percent">{{ round($m_maintenance, 2) }}%</tspan>   
+                        </text>
+                        <text y="60%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-data">% dari Anggaran Pengadaan</tspan>   
+                        </text>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+                <div class="col text-center">
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Realisasi</div>
+                      <div><strong>{{ rupiah($m_realisasi) }}</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Anggaran</div>
+                      <div><strong>{{ rupiah($m_anggaran) }}</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Selisih</div>
+                      <div><strong>{{ rupiah($m_anggaran - $m_realisasi) }}</strong></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
