da9160ae0e4f543344fe2b2f1c7388db5d29542d
FieryInferno <bagassetia271@gmail.com>
2021-09-23 17:08:46 +0700

    Perubahan tampilan dashboard admin

--- resources/views/wadek/dashboard.blade.php
+++ resources/views/wadek/dashboard.blade.php
@@ -163,5 +163,103 @@
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="col-12">
+      <div class="row">
+        <div class="col-6">
+          <div class="card">
+            <div class="card-header">Pengeluaran Pengadaan</div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <div class="svg-item">
+                    <svg width="350%" height="100%" viewBox="0 0 40 40" class="donut">
+                      <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+                      <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+                      <circle class="donut-segment donut-segment-2" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="50 50" stroke-dashoffset="25"></circle>
+                      <g class="donut-text donut-text-1">
+                        <text y="50%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-percent">50%</tspan>   
+                        </text>
+                        <text y="60%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-data">% dari Anggaran Pengadaan</tspan>   
+                        </text>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+                <div class="col text-center">
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Realisasi</div>
+                      <div><strong>Rp. 6.000,00</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Anggaran</div>
+                      <div><strong>Rp. 6.000,00</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Selisih</div>
+                      <div><strong>Rp. 6.000,00</strong></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="card">
+            <div class="card-header">Pengeluaran Pengadaan</div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  <div class="svg-item">
+                    <svg width="350%" height="100%" viewBox="0 0 40 40" class="donut">
+                      <circle class="donut-hole" cx="20" cy="20" r="15.91549430918954" fill="#fff"></circle>
+                      <circle class="donut-ring" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5"></circle>
+                      <circle class="donut-segment donut-segment-2" cx="20" cy="20" r="15.91549430918954" fill="transparent" stroke-width="3.5" stroke-dasharray="50 50" stroke-dashoffset="25"></circle>
+                      <g class="donut-text donut-text-1">
+                        <text y="50%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-percent">50%</tspan>   
+                        </text>
+                        <text y="60%" transform="translate(0, 2)">
+                          <tspan x="50%" text-anchor="middle" class="donut-data">% dari Anggaran Pengadaan</tspan>   
+                        </text>
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+                <div class="col text-center">
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Realisasi</div>
+                      <div><strong>Rp. 6.000,00</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Anggaran</div>
+                      <div><strong>Rp. 6.000,00</strong></div>
+                    </div>
+                  </div>
+                  <div class="row bg-secondary">
+                    <div class="col border p-1">
+                      <div>Selisih</div>
+                      <div><strong>Rp. 6.000,00</strong></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
